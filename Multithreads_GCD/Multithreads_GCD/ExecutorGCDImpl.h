f6a64ab6f153a3d11ee2f8ce307f10853b42d9a6
skyjasmine <1139577434@qq.com>
2020-06-19 19:36:16 +0800

    Update and rename ExecutorGCdImpl.h to ExecutorGCDImpl.h
    change "Gcd" to "GCD".

--- Multithreads_GCD/Multithreads_GCD/ExecutorGCDImpl.h
+++ Multithreads_GCD/Multithreads_GCD/ExecutorGCDImpl.h
@@ -3,7 +3,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ExecutorGCdImpl : NSObject<Executor>
+@interface ExecutorGCDImpl : NSObject<Executor>
 
 @property (nonatomic,strong) dispatch_queue_t queue;
 
